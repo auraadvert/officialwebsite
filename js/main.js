ec7bb88963311260f308e9a1bf48e254394969d3
Karan <karanjohar008@gmail.com>
2022-01-06 00:31:56 +0530

    slider added to testimonial

--- js/main.js
+++ js/main.js
@@ -351,3 +351,23 @@ jQuery(document).ready(function($) {
   siteIstotope();
 
 });
+
+
+      $('.owl-carousel').owlCarousel({
+          loop:true,
+          margin:10,
+          nav:true,
+          dots: false,
+          navText: ["<img src='images/leftarrow.png'>","<img src='images/rightarrow.png'>"],
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:1
+              },
+              1000:{
+                  items:3
+              }
+          }
+      })
